2d0dd60315ae1bebe8eb5fd852a64752eb03a33e
Bo Rie <bo.panda.qt@gmail.com>
2017-08-10 10:29:33 +0700

    fix

--- app/controllers/v1/timesheets_controller.rb
+++ app/controllers/v1/timesheets_controller.rb
@@ -2,18 +2,23 @@ class V1::TimesheetsController < V1::BaseController
   skip_before_filter :verify_authenticity_token, only: [:create, :update]
   before_action :load_timesheet, only: :update
 
+  def index
+    response_success "OK", Timesheet.all
+  end
+
   def create
-    if Timesheet.create project_id: current_user.projects.first.id,
+    timesheet = Timesheet.new project_id: current_user.projects.first.id,
       user_id: current_user.id, start: DateTime.now, end: DateTime.now
-      response_success "Khởi tạo thành công."
+    if timesheet.save
+      response_success "Khởi tạo thành công.", timesheet
     else
       response_error "Khởi tạo thất bại."
     end
   end
 
   def update
-    if @timesheet.update_attribute :time_end, DateTime.now
-      response_success "Cập nhật thành công."
+    if @timesheet.update_attribute :end, DateTime.now
+      response_success "Cập nhật thành công.", @timesheet
     else
       response_eror "Cập nhật thất bại."
     end
