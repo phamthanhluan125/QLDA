b97b0e8f185e25b5b48fbc4aadfa07f576ee8629
Bo Rie <bo.panda.qt@gmail.com>
2017-08-10 10:27:19 +0700

    fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthday, :gender, :avatar, :authentication_token,
+  attributes :id, :name, :email, :time_scr, :birthday, :gender, :avatar, :authentication_token,
     :address, :status, :created_at
   belongs_to :role
 end
